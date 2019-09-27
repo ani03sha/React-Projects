7d31ce06e715e57570fa35404f9c906e239300cd
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-09-27 22:34:52 +0530

    Added function to load data from API

--- react-tutorial-tania-rascia/src/index.js
+++ react-tutorial-tania-rascia/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './App' // Use this for Form data
+// import App from './Api'   // Use this for data from API
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
