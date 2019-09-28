79874cb0bc1334b29e803249f09c482bfd9f1052
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-09-28 21:15:41 +0530

    Added delete user functionality

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -15,6 +15,10 @@ const App = () => {
     setUsers([...users, user])
   }
 
+  const deleteUser = id => {
+    setUsers(users.filter(user => user.id !== id))
+  }
+
   const [users, setUsers] = useState(usersData)
 
   return(
@@ -27,7 +31,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View Users</h2>
-          <UserTable users={users} />
+          <UserTable users={users} deleteUser={deleteUser} />
         </div>
       </div>
     </div>
