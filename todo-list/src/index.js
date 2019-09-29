676124129525e1ce925931219675ab214e943523
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-09-29 20:59:01 +0530

    Added skeleton of Todo List project

---
+++ todo-list/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+var destination = document.querySelector('#root');
+
+ReactDOM.render(<div><p>Hello!</p></div>, destination);
