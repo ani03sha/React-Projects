145e0c36b87c698b8690c8c93825852514892afe
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-10-03 22:48:17 +0530

    Added the dynamic API call

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,15 +5,44 @@ import Weather from './components/Weather';
 
 class App extends Component {
 
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+
   getWeather = async (e) => {
 
     e.preventDefault();
 
-    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=API_KEY');
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    console.log(city);
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=API_KEY`);
 
     const response = await api_call.json();
 
     console.log(response);
+
+    if(city && country) {
+      this.setState({
+        temperature: response.main.temp,
+        city: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        error: ""
+      })
+    } else {
+      this.setState({
+        error: "Please input search values..."
+      })
+    }
   }
 
 
@@ -22,7 +51,13 @@ class App extends Component {
         <div>
           <Title />
           <Form loadWeather={this.getWeather}/>
-          <Weather />
+          <Weather 
+            temperature={this.state.temperature}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            description={this.state.description}
+            error={this.state.error} />
         </div>
       )
   }
